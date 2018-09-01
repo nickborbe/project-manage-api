10b59960830fb3b21e7b69add6f459ef11a9ad0a
nickborbe <nickborbe@gmail.com>
2018-09-01 17:47:56 -0400

    add project routes

---
+++ routes/projects.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router  = express.Router();
+const Project    = require('../models/Project');
+
+
+router.get('/projects', (req, res, next) => {
+    Project.find()
+    .then((allTheProjects)=>{
+        res.json(allTheProjects);
+    })
+    .catch((err)=>{
+        res.json(err);
+    })
+});
+
+router.post('/projects/create', (req, res, next)=>{
+    if(!req.user){
+       return res.json({message: 'sorry, you must be logged in to create a Project'}) 
+    }
+    Project.create({
+        title: req.body.title,
+        description: req.body.description,
+        doneyet: req.body.doneyet,
+        owner: req.user._id,
+    })
+    .then((response)=>{
+        res.json(response);
+    })
+    .catch((err)=>{
+        res.json(err);
+    })
+})
+
+router.post('/projects/edit/:id', (req, res, next)=>{
+    Project.findByIdAndUpdate(req.params.id, req.body)
+    .then((response)=>{
+        res.json(response)
+    })
+    .catch((err)=>{
+        res.json(err)
+    })
+})
+
+router.post('/projects/delete/:id', (req, res, next)=>{
+    Project.findByIdAndRemove(req.params.id)
+    .then((response)=>{
+        res.json(response)
+    })
+    .catch((err)=>{
+        res.json(err)
+    })
+
+})
+
+
+
+
+
+
+
+
+module.exports = router;
